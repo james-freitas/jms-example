339bbe376f70b84b91c7b35eae477119b7ae98fc
James Freitas <jamesfrj@yahoo.com.br>
2021-03-30 20:38:24 -0300

    Receiving JMS messages

---
+++ src/main/java/com/codeonblue/jms/listener/HelloMessageListener.java
@@ -0,0 +1,24 @@
+package com.codeonblue.jms.listener;
+
+import com.codeonblue.jms.config.JmsConfig;
+import com.codeonblue.jms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) {
+
+        System.out.println("I got a Message!!!");
+        System.out.println(helloWorldMessage);
+
+    }
+}
