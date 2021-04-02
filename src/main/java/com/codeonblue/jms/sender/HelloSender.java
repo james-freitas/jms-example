d93cbd2e4019c1318a86b7bffd23d7da92bd3d8e
James Freitas <jamesfrj@yahoo.com.br>
2021-04-02 12:52:33 -0300

    Send and receive of JMS messages

--- src/main/java/com/codeonblue/jms/sender/HelloSender.java
+++ src/main/java/com/codeonblue/jms/sender/HelloSender.java
@@ -2,11 +2,17 @@ package com.codeonblue.jms.sender;
 
 import com.codeonblue.jms.config.JmsConfig;
 import com.codeonblue.jms.model.HelloWorldMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -14,10 +20,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
-        System.out.println("I'm sending a message");
 
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
@@ -26,7 +32,35 @@ public class HelloSender {
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+    }
 
-        System.out.println("Message sent");
+    @Scheduled(fixedRate = 2000)
+    public void sendReceiveMessage() throws JMSException {
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "com.codeonblue.jms.model.HelloWorldMessage");
+
+                    System.out.println("Sending hello");
+
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+
+            }
+        });
+
+        System.out.println(receivedMsg.getBody(String.class));
     }
 }
