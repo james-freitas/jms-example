03f17955f3efd72faf9e12607ae41e598fb4c793
James Freitas <jamesfrj@yahoo.com.br>
2021-04-04 09:57:50 -0300

    Using Local ActiveMQ Broker with Spring Boot

--- src/main/java/com/codeonblue/jms/JmsApplication.java
+++ src/main/java/com/codeonblue/jms/JmsApplication.java
@@ -1,8 +1,5 @@
 package com.codeonblue.jms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,14 +9,14 @@ public class JmsApplication {
 	public static void main(String[] args) throws Exception{
 
 
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-			.setPersistenceEnabled(false)
-			.setJournalDirectory("target/data/journal")
-			.setSecurityEnabled(false)
-			.addAcceptorConfiguration("invm", "vm://1")
-		);
+//		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//			.setPersistenceEnabled(false)
+//			.setJournalDirectory("target/data/journal")
+//			.setSecurityEnabled(false)
+//			.addAcceptorConfiguration("invm", "vm://1")
+//		);
 
-		server.start();
+//		server.start();
 
 		SpringApplication.run(JmsApplication.class, args);
 	}
